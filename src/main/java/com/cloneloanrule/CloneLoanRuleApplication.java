6200ff0fa8b94287bb58ca29048aaa233ec19341
ymzkjpx <ymzkjpx@gmail.com>
2021-11-16 18:54:38 +0900

    first commit

---
+++ src/main/java/com/cloneloanrule/CloneLoanRuleApplication.java
@@ -0,0 +1,13 @@
+package com.cloneloanrule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneLoanRuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneLoanRuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloneloanrule/CloneLoanRuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloneloanrule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneLoanRuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
