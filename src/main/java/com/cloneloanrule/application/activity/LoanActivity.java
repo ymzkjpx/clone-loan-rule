a3e3b91937d820a9b153d918415546c811819268
ymzkjpx <ymzkjpx@gmail.com>
2021-11-17 12:35:47 +0900

    貸出状況確認機能を実装

--- src/main/java/com/cloneloanrule/application/activity/LoanActivity.java
+++ src/main/java/com/cloneloanrule/application/activity/LoanActivity.java
@@ -1,16 +1,32 @@
 package com.cloneloanrule.application.activity;
 
 import com.cloneloanrule.application.collection.CollectionService;
+import com.cloneloanrule.application.loan.LoanService;
+import com.cloneloanrule.application.member.MemberService;
 import com.cloneloanrule.domain.collection.Entries;
+import com.cloneloanrule.domain.loan.LoanContext;
+import com.cloneloanrule.domain.loan.LoanHistory;
+import com.cloneloanrule.domain.member.Member;
+import com.cloneloanrule.domain.member.MemberNumber;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoanActivity {
     CollectionService collectionService;
+    MemberService memberService;
+    LoanService loanService;
 
-    public LoanActivity(CollectionService collectionService) {
+    public LoanActivity(CollectionService collectionService, MemberService memberService, LoanService loanService) {
         this.collectionService = collectionService;
+        this.memberService = memberService;
+        this.loanService = loanService;
+    }
+
+    public LoanContext 貸出状況(MemberNumber 会員番号){
+        Member 会員 = memberService.会員(会員番号);
+        LoanHistory 貸出履歴 = loanService.貸出履歴(会員番号);
+        return LoanContext.of(会員, 貸出履歴);
     }
 
     public Entries 蔵書一覧(){
